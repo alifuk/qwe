79d674c690d06ac90dd42e64e759ee851962aa2f
Albert Myšák <ays@hud.cz>
2019-04-24 10:08:37 +0200

    img load from file

--- qwe/mainpage.cpp
+++ qwe/mainpage.cpp
@@ -1,6 +1,7 @@
 #include "mainpage.h"
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
 
 
 Mainpage::Mainpage(QWidget *parent)
@@ -8,20 +9,17 @@ Mainpage::Mainpage(QWidget *parent)
 {
 	ui.setupUi(this);
 
-	cv::Mat* img = new cv::Mat(1000, 2000, CV_8UC1, cv::Scalar(80));
-
+	//cv::Mat* img = new cv::Mat(1000, 2000, CV_8UC1, cv::Scalar(80));
+	cv::Mat img;
+	img = cv::imread("computar.png",0);
 	//cv::Mat temp; // make the same cv::Mat
 	//cvtColor(img, temp, CV_BGR2RGB); // cvtColor Makes a copt, that what i need
 	
-	QImage dest((const uchar *)(*img).data, (*img).cols, (*img).rows, (*img).step, QImage::Format_Grayscale8);
-	dest.bits();
 
 	QGraphicsScene* scene = new QGraphicsScene();
-	QPixmap* pixmap = new QPixmap("C:/img.jpg");
-	//QPixmap* pixmap = QPixmap::fromImage( dest );
-	//QPixmap pixmap = QPixmap::fromImage(dest);
-	//scene.addPixmap(QPixmap::fromImage(dest));
-	scene->addPixmap(*pixmap);
+	QPixmap p = QPixmap::fromImage(QImage(img.data, img.cols, img.rows, QImage::Format_Grayscale8));
+	QPixmap* pi = &p;
+	scene->addPixmap(*pi);
 
 	ui.graphicsView->setScene(scene);
 	
