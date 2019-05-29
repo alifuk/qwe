9545976c73501b9dbc12ebb55d9a04b5cee61506
Albert <amdalifuk@gmail.com>
2019-05-29 08:33:32 +0200

    api buildup

--- qwe/mainpage.h
+++ qwe/mainpage.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+
 #include "ui_main_page.h"
+#include "krokosled.h"
+
+using namespace std;
 
 class Mainpage : public QMainWindow
 {
@@ -9,15 +13,21 @@ class Mainpage : public QMainWindow
 
 public:
 	Mainpage(QWidget *parent = Q_NULLPTR);
+	krokosled getSelectedState();
 
 public slots:
-	void muj();
 	void fit();
-	void load_image();
+	void add_image();
+	void add_step();
+	void add_state();
+	void delete_state();
 private:
 	Ui::MainpageClass ui;
 	QGraphicsScene* scene;
 	float f_zoom;
+	vector<krokosled> states;
+	int number_of_states;
+
 protected:
 	bool Mainpage::eventFilter(QObject *obj, QEvent *event); 
 };
